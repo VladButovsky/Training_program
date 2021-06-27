3986b01bd1e1b0b7324eda599ba97b335cb694c6
VladButovsky <83472569+VladButovsky@users.noreply.github.com>
2021-06-27 15:29:47 +0300

    Создал правильную прогрессию нагрузок на 8 недель для жима лежа .

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # my_exercises = []
-# max_chest_press = my_exercises.append(int(input('Введите ваш жим с груди')))
+# max_chest_press = (int(input('Введите ваш жим с груди')))
 # max_squats = int(input('Введите присед'))
 # standing_chest_press = int(input('Ведите жим с груди стоя'))
 # lifting_biceps = int(input('Ведите подъем на бицепс штанги'))
@@ -8,7 +8,7 @@
 # lat_pull_down_machine = int(input('Ведите вашу тягу в вертикальном блоке '))
 # max_romanian_lift = int(input('Ведите вашу румынскую тягу'))
 # shoulders = int(input('Ведите вес гантелей для упражения махи плечами'))
-# easy_max_romanian_lift = int(input('Ведите вашу румынскую тягу'))
+# max_romanian_lift = int(input('Ведите вашу румынскую тягу'))
 
 # easy_shoulders = shoulders//100*50
 # heavy_lat_pull_down_machine = lat_pull_down_machine//100*85
@@ -30,27 +30,68 @@
 # heavy_day_chest_press = max_chest_press//100*85
 
 
-# print(my_exercises)
-# easy_load = []
-# for week, weight in enumerate(my_exercises):
-#     week = 1
-#
-#     ('неделя',week, 'вес жима',int(weight / 100 * 50))
-
 class ChestPress:
-    def __init__(self,max_chest_press):
+    def __init__(self, max_chest_press):
         self.weight = max_chest_press
         self.easy = int(max_chest_press / 100 * 50)
         self.med = int(max_chest_press / 100 * 70)
         self.heavy = int(max_chest_press / 100 * 85)
-        i = 0
-        for weight in range(1,8):
-            self.easy = int(max_chest_press / 100 * 50+i)
-            self.med = int(max_chest_press / 100 * 70+i)
-            self.heavy = int(max_chest_press / 100 * 85+i)
-            result = print('Жим лежа', 'легкая тренировка',self.easy,'средняя тренировка',self.med,'тяжелая тренировка',self.heavy)
-            i += 10
+        easy = 0
+        medium = 0
+        hard = 0
+        for week in range(1, 8):
+            if week == 1:
+                easy += 0
+                medium += 0
+                hard += 0
+                self.easy = int(max_chest_press / 100 * 50 + easy)
+                self.med = int(max_chest_press / 100 * 70 + medium)
+                self.heavy = int(max_chest_press / 100 * 85 + hard)
+                print('Жим лежа', 'легкая тренировка', self.easy, 'средняя тренировка', self.med,
+                      'тяжелая тренировка', self.heavy)
 
-        return result
+            elif week == 2:
+                easy += 5
+                medium += 2
+                hard += 7
+                self.easy = int(max_chest_press / 100 * 50 + easy)
+                self.med = int(max_chest_press / 100 * 70 + medium)
+                self.heavy = int(max_chest_press / 100 * 85 + hard)
+                print('Жим лежа', 'легкая тренировка', self.easy, 'средняя тренировка', self.med,
+                      'тяжелая тренировка', self.heavy)
 
-press_chest_program = ChestPress(120)
+            elif week == 3:
+                easy += 5
+                medium =+ 2
+                hard += 8
+                self.easy = int(max_chest_press / 100 * 50 + easy)
+                self.med = int(max_chest_press / 100 * 70 + medium)
+                self.heavy = int(max_chest_press / 100 * 85 + hard)
+                print('Жим лежа', 'легкая тренировка', self.easy, 'средняя тренировка', self.med,
+                      'тяжелая тренировка', self.heavy)
+
+            elif week == 4:
+                easy += 2
+                medium += 2
+                hard += 2
+                self.easy = int(max_chest_press / 100 * 50 + easy)
+                self.med = int(max_chest_press / 100 * 70 + medium)
+                self.heavy = int(max_chest_press / 100 * 85 + hard)
+                print('Жим лежа', 'легкая тренировка', self.easy, 'средняя тренировка', self.med,
+                      'тяжелая тренировка', self.heavy)
+
+            elif week >= 5:
+                easy += 2
+                medium += 2
+                hard += 2
+                self.easy = int(max_chest_press / 100 * 50 + easy)
+                self.med = int(max_chest_press / 100 * 70 + medium)
+                self.heavy = int(max_chest_press / 100 * 85 + hard)
+                print('Жим лежа', 'легкая тренировка', self.easy, 'средняя тренировка', self.med,
+                           'тяжелая тренировка', self.heavy)
+
+
+
+
+
+press_chest_program = ChestPress(80)
