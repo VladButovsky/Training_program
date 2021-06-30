af545ad51df14cbb43826b7c3f2e6a5e7801bda4
VladButovsky <83472569+VladButovsky@users.noreply.github.com>
2021-06-30 13:56:21 +0300

    Есть класс армейского жима и правильная прогрессия нагрузок к нему

--- main_2.py
+++ main_2.py
@@ -1,6 +1,6 @@
 # max_chest_press = (int(input('Введите ваш жим с груди')))
 # max_squats = int(input('Введите присед'))
-# standing_chest_press = int(input('Ведите жим с груди стоя'))
+# max_standing_chest_press = int(input('Ведите жим с груди стоя'))
 # lifting_biceps = int(input('Ведите подъем на бицепс штанги'))
 # bent_over_row = int(input('Ведите тягу штанги в наклоне '))
 # close_grip_bench_press = int(input('Ведите жим лежа узким хватом '))
@@ -22,7 +22,7 @@ class TrainingCalculator:
         easy = 0
         medium = 0
         hard = 0
-        for week in range(0, 9):
+        for week in range(1, 9):
             if week == 1:
                 easy += 0
                 medium += 0
@@ -70,9 +70,7 @@ class TrainingCalculator:
             # print('Неделя {} Легкая тренировка {}. Средняя тренировка {}.Тяжелая тренировка {}'.format(week,self.easy, self.med,
             #                                                                                  self.heavy))
 
-
 squat_list = []
-
 
 class Squat:
     def __init__(self, max_squats):
@@ -83,16 +81,10 @@ class Squat:
         easy = 0
         medium = 0
         hard = 0
-        for week in range(0, 9):
+        for week in range(1, 9):
             if week == 1:
-                easy += 0
-                medium += 0
-                hard += 0
-                self.easy += easy
                 squat_list.append(self.easy)
-                self.med += medium
                 squat_list.append(self.med)
-                self.heavy += hard
                 squat_list.append(self.heavy)
                 squat_list.append('-')
             elif week == 2:
@@ -128,22 +120,50 @@ class Squat:
                 self.heavy = int(self.weight / 100 * 85 + hard)
                 squat_list.append(self.heavy)
                 squat_list.append('-')
-            # print('Неделя {} Легкая тренировка {}. Средняя тренировка {}.Тяжелая тренировка {}'.format(week, self.easy,
-            #                                                                                            self.med,
-            #                                                                                            self.heavy))
+
+standing_chest_press_list = []
+
+class StandingChestPress:
+    def __init__(self, max_standing_chest_press):
+        self.weight = max_standing_chest_press
+        self.easy = int(self.weight / 100 * 50)
+        self.med = int(self.weight / 100 * 70)
+        self.heavy = int(self.weight / 100 * 85)
+        easy = 0
+        medium = 0
+        hard = 0
+        for week in range(1, 9):
+                easy += .5
+                medium += .5
+                hard += .5
+                self.easy += easy
+                standing_chest_press_list.append(self.easy)
+                self.med += medium
+                standing_chest_press_list.append(self.med)
+                self.heavy += hard
+                standing_chest_press_list.append(self.heavy)
+                standing_chest_press_list.append('-')
+
 
 
 press_chest_program = TrainingCalculator(max_chest_press=80)
 squat_program = Squat(max_squats=90)
+standing_chest_press_program = StandingChestPress(max_standing_chest_press = 35)
+
 print('chest press', chest_press_list)
 print('squad', squat_list)
-
+print('standing chest press', standing_chest_press_list)
 
 class TrainingProgramPrinter:
-    def print_program(self, chest_press_list,squat_list):
-        print('1 неделя \nДень 1 \nЖим лежа. Вес:', chest_press_list[0], 'кг. 4х12\nПрисед. Вес:',squat_list[2],'кг. 3х8')
+    def print_program(self, chest_press_list, squat_list,standing_chest_press_list):
+        print('1 неделя '
+              '\nДень 1 '
+              '\nЖим лежа. Вес:', chest_press_list[1], 'кг. 4х12'
+              '\nПрисед. Вес:', squat_list[2],'кг. 3х8'
+              '\nАрмейский жим. Вес:',standing_chest_press_list[1],'кг. 3х10'
+              '\n')
 
 
-# print('1 тернировка, 1 неделя, \n 1 - жим, 4х12', chest_press_list[0])
+
 week_1 = TrainingProgramPrinter()
-week_1.print_program(chest_press_list,squat_list)
+week_1.print_program(chest_press_list, squat_list,standing_chest_press_list)
